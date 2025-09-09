b07fd06ab127b6a743b2267526ba895940a2c63a
KenTree <kennethly909808@csu.fullerton.edu>
2025-09-09 01:00:34 -0700

    Fixed for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/KenTree-Website/',  // 👈 important!
+  base: '/' // <-- this ensures Vite generates paths from root
 })
